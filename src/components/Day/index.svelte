fb506391b20fbb01e99f239ececa11db88d48ff6
wicky1510 <r.admiraal@gen25.com>
2020-05-31 17:48:09 +0200

    initial commit

---
+++ src/components/Day/index.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+    export let i;
+
+    function activateDay() {
+        dispatch('changeDay', {
+            day: i
+        });
+    }
+
+    import moment from 'moment';
+    export let timestamp;
+    const momentDate = moment.unix(timestamp);
+</script>
+
+<button class="day__button" on:click={activateDay}>
+    <span class="date__text">{momentDate.format('MMM DD')}</span>
+    <span class="day__text">{momentDate.format('dddd')}</span>
+</button>
---
+++ src/components/DayOverlay/index.svelte
@@ -0,0 +1,10 @@
+<script>
+    import moment from 'moment';
+    export let timestamp;
+    const momentDate = moment.unix(timestamp);
+</script>
+
+<span class="day__button">
+    <span class="date__text">{momentDate.format('MMM DD')}</span>
+    <span class="day__text">{momentDate.format('dddd')}</span>
+</span>
